4ca4663abd8098c2e1879413fbd65903b7bd7456
script <slmsttrv1@gmail.com>
2022-09-12 15:35:41 +0500

    add: test section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import SectionTest from "./Components/SectionTest/SectionTest";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SectionTest />
     </div>
   );
 }
