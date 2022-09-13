6853ab8c07b49139b30bbda7314d1eb077c2ebe3
usmonzo <turaef.usmon@gmail.com>
2022-09-14 00:58:45 +0500

    Adding final block with phone img.

--- src/Components/Block/Block.jsx
+++ src/Components/Block/Block.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import './Block.css'
+import React from "react";
+import "./Block.css";
+import gglplay from "./gglplay.svg";
+import appstore from "./appstore.svg";
+import appgallery from "./appgallery.svg";
+import phone from "./phone.svg";
 
 const Block = () => {
-    return (
-        <div className="container">
-            <div className='header-promo-block'>
-                <div className="header-promo-block-content">
-                    <h1 className="header-promo-block-title">Установите <span className='.span_item'>Хумо Онлайн,</span>
-                        чтобы управлять своим депозитом</h1>
-                    <h3 className="header-promo-block-subtitle">
-                        Получайте до 17% годовых
-                    </h3>
-                    <button className="header-promo-btn">Открыть вклад</button>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="header-promo-block">
+        <div className="block-content">
+          <h1 className="header-promo-block-title">
+            Установите <span className=".span_item">Хумо Онлайн, </span>
+            чтобы управлять своим депозитом
+          </h1>
+          <div className="block-btns">
+            <img src={gglplay} className="block-btn-btn" alt="ggl" />
+            <img src={appstore} className="block-btn-btn" alt="ggl" />
+            <img src={appgallery} className="block-btn-btn" alt="ggl" />
+          </div>
         </div>
-    )
-}
+        <img src={phone} className="block-phone-img" alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Block
+export default Block;
