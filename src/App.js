f50522e21fad335444e1b10e128f20f59f8abdd4
usmonzo <turaef.usmon@gmail.com>
2022-09-14 15:00:44 +0500

    ..,

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Block from './Components/Block/Block';
+import Block from "./Components/Block/Block";
 import Card from "./Components/Card/Card";
 import cardImage from "./Components/Card/illustration.png";
 import Footer from "./Components/Footer/Footer";
@@ -7,17 +7,15 @@ import FooterNavbar from "./Components/Footer/FooterNavbar";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import NumberedCard from "./Components/NumberedCard/NumberedCard";
-import WrapperImg1 from './Components/WrapperCard/card1.png';
-import WrapperImg2 from './Components/WrapperCard/card2.png';
-import Wrapper from './Components/WrapperCard/WrapperCard';
-
-
+import WrapperImg1 from "./Components/WrapperCard/card1.png";
+import WrapperImg2 from "./Components/WrapperCard/card2.png";
+import Wrapper from "./Components/WrapperCard/WrapperCard";
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="container">
-        <Navbar />
         <Header />
         <section className="card-items">
           <Card src={cardImage} title="Ставка 17%" subtitle="годовых" />
@@ -55,13 +53,13 @@ function App() {
       <h2 className="card-title">Пополняйте вклад, как вам удобно</h2>
       <section className="card-items">
         <Wrapper
-          title='Пополнение через Хумо онлайн'
-          subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+          title="Пополнение через Хумо онлайн"
+          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
           src={WrapperImg1}
         />
         <Wrapper
-          title='Пополнение через отделения Хумо'
-          subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+          title="Пополнение через отделения Хумо"
+          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           src={WrapperImg2}
         />
       </section>
@@ -69,14 +67,16 @@ function App() {
       <section className="card-items">
         <NumberedCard
           num={1}
-          titleSecond='Заполните онлайн-заявку и посетите банк' />
+          titleSecond="Заполните онлайн-заявку и посетите банк"
+        />
         <NumberedCard
           num={2}
-          titleSecond='Заполните онлайн-заявку и посетите банк'
+          titleSecond="Заполните онлайн-заявку и посетите банк"
         />
         <NumberedCard
           num={3}
-          titleSecond='Заполните онлайн-заявку и посетите банк' />
+          titleSecond="Заполните онлайн-заявку и посетите банк"
+        />
       </section>
       <Block />
       <FooterNavbar />
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,36 +4,38 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="navbar">
-        <a href="https://humo.tj/ru/" target="_blank" rel="noreferrer">
-          <img className="navbar-logo" src={humoLogo} alt="#" />
-        </a>
-        <ul className="navbar-links">
-          <li>
-            <a className="nav-link" href="#top">
-              Преимущества
-            </a>
-          </li>
-          <li>
-            <a className="nav-link" href="#top">
-              Калькулятор
-            </a>
-          </li>
-          <li>
-            <a className="nav-link" href="#top">
-              Пополнение
-            </a>
-          </li>
-          <li>
-            <a className="nav-link" href="#top">
-              Открытие вклада
-            </a>
-          </li>
-        </ul>
-        <span className="tel-number">
-          <a href="tel:+992(44)6405544">+992 (44) 640 55 44</a>
-        </span>
+    <div className=" navbar-container">
+      <div className="container">
+        <div className="navbar">
+          <a href="https://humo.tj/ru/" target="_blank" rel="noreferrer">
+            <img className="navbar-logo" src={humoLogo} alt="#" />
+          </a>
+          <ul className="navbar-links">
+            <li>
+              <a className="nav-link" href="#top">
+                Преимущества
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#top">
+                Калькулятор
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#top">
+                Пополнение
+              </a>
+            </li>
+            <li>
+              <a className="nav-link" href="#top">
+                Открытие вклада
+              </a>
+            </li>
+          </ul>
+          <span className="tel-number">
+            <a href="tel:+992(44)6405544">+992 (44) 640 55 44</a>
+          </span>
+        </div>
       </div>
     </div>
   );
