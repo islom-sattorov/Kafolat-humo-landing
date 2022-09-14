b11c3608b141b063c1b0c91b2292bb5dc887a242
usmonzo <turaef.usmon@gmail.com>
2022-09-14 14:39:49 +0500

    ...

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import style from './Card.module.css'
+import React from "react";
+import style from "./Card.module.css";
 
 const SectionTest = (props) => {
-    return (
-        <div className={style.card__item}>
-            <img src={props.src} alt='' />
-            <h3 className={style.title}>{props.title}</h3>
-            <p className={style.subtitle}>{props.subtitle}</p>
+  return (
+    <div className="container">
+      <div className={style.card__item}>
+        <div className="card-content">
+          <img src={props.src} alt="" />
+          <h3 className={style.title}>{props.title}</h3>
+          <p className={style.subtitle}>{props.subtitle}</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SectionTest
+export default SectionTest;
