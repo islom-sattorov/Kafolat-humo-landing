8c6655d02b80a01f09266c555a1584dd89c302d8
islom-sattorov <slmsttrv1@gmail.com>
2022-11-30 12:14:43 +0500

    cards

--- src/Components/Protection/Protecton.jsx
+++ src/Components/Protection/Protecton.jsx
@@ -5,18 +5,18 @@ import './Protecton.css';
 
 export default function Protection() {
     return (
-        <div className="container">
-            <section className="protec-container">
-                <article className="protec-image">
-                    <LazyLoadImage src={Logo} alt="Logo" />
-                </article>
-                <article className="protec-content">
-                    <h4>Хумо - член Фонда страхования сбережений физических лиц</h4>
-                    <p>C 2013 года ЗАО МДО “Хумо” является членом Фонда страхования сбережений физических лиц.
-                        Ваши вклады будут застрахованы в соответствии с Законом Республики Таджикистан «О страховании вкладов физических лиц».
-                        Более подробная информация на сайте <a className="protec-link" target='_blank' rel="noopener noreferrer" href="http://idif.tj/">www.idif.tj</a></p>
-                </article>
-            </section>
-        </div>
+        // <div className="container">
+        <section className="protec-container">
+            <article className="protec-image">
+                <LazyLoadImage src={Logo} alt="Logo" />
+            </article>
+            <article className="protec-content">
+                <h4>Хумо - член Фонда страхования сбережений физических лиц</h4>
+                <p>C 2013 года ЗАО МДО “Хумо” является членом Фонда страхования сбережений физических лиц.
+                    Ваши вклады будут застрахованы в соответствии с Законом Республики Таджикистан «О страховании вкладов физических лиц».
+                    Более подробная информация на сайте <a className="protec-link" target='_blank' rel="noopener noreferrer" href="http://idif.tj/">www.idif.tj</a></p>
+            </article>
+        </section>
+        // </div>
     );
 }
